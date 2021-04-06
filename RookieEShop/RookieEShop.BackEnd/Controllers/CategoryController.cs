134f1acd232aa69287fdc69d766e7b49993f9d3b
DESKTOP-FT4H1LE\Sergio Nguyen <hoangnguyen12397@gmail.com>
2021-04-06 18:07:25 +0700

    Add Category API

---
+++ RookieEShop/RookieEShop.BackEnd/Controllers/CategoryController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RookieEShop.BackEnd.Data;
+using RookieEShop.BackEnd.Models;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+	{
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<CategoryVm>>> GetCategories()
+        {
+            return await _context.Categories
+                .Select(x => new CategoryVm { Id = x.Id, Name = x.Name })
+                .ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<CategoryVm>> GetCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            var categoryVm = new CategoryVm
+            {
+                Id = category.Id,
+                Name = category.Name
+            };
+
+            return categoryVm;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategory(int id, CategoryCreateRequest categoryCreateRequest)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            category.Name = categoryCreateRequest.Name;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CategoryVm>> PostCategory(CategoryCreateRequest categoryCreateRequest)
+        {
+            var category = new Category
+            {
+                Name = categoryCreateRequest.Name
+            };
+
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCategory", new { id = category.Id }, new CategoryVm { Id = category.Id, Name = category.Name });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
