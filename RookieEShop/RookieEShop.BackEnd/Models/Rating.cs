7812c3484f8cf4798bfe5c33c864c24c982c05cf
DESKTOP-FT4H1LE\Sergio Nguyen <hoangnguyen12397@gmail.com>
2021-04-09 15:56:18 +0700

    Update Db and add Rating model

---
+++ RookieEShop/RookieEShop.BackEnd/Models/Rating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Models
+{
+	public class Rating
+	{
+		public int Id { get; set; }
+
+		public int Val { get; set; }
+
+		[ForeignKey("User")]
+		public string UserId { get; set; }
+
+		public User User { get; set; }
+		[ForeignKey("Product")]
+
+		public int ProductId { get; set; }
+
+		public Product Product { get; set; }
+	}
+}
