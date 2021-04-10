02e80b7d5e2d63ebf9d43b32605a6482926d06e1
DESKTOP-FT4H1LE\Sergio Nguyen <hoangnguyen12397@gmail.com>
2021-04-10 16:57:51 +0700

    Update Product database

---
+++ RookieEShop/RookieEShop.BackEnd/Migrations/20210409094417_updateProductDb.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RookieEShop.BackEnd.Migrations
+{
+    public partial class updateProductDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Publisher",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Publisher",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Products");
+        }
+    }
+}
