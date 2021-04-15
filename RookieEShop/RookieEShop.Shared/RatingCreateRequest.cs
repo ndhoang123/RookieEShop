266b4a77279fcb597e95793ac3f4bd372542fe9e
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-15 13:18:42 +0700

    Add Rating in sharedview

---
+++ RookieEShop/RookieEShop.Shared/RatingCreateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.Shared
+{
+	class RatingCreateRequest
+	{
+		public int Id { get; set; }
+		public int ProductId { get; set; }
+	}
+}
---
+++ RookieEShop/RookieEShop.Shared/RatingVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.Shared
+{
+	class RatingVm
+	{
+		public int Id { get; set; }
+		public int Val { get; set; }
+		
+	}
+}
