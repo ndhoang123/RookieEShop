9e46b31ded81f3db043ec4b9af8f69d7bd94536d
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-15 19:06:37 +0700

    Add Post for Rating in product detail

--- RookieEShop/RookieEShop.FrontEnd/Controllers/ProductController.cs
+++ RookieEShop/RookieEShop.FrontEnd/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using RookieEShop.FrontEnd.Services;
+using RookieEShop.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,11 @@ namespace RookieEShop.FrontEnd.Controllers
 	public class ProductController : Controller
 	{
 		private readonly IProductApiClient _productApiClient;
-		public ProductController(IProductApiClient productApiClient)
+		private readonly IRatingApiClient _ratingApiClient;
+		public ProductController(IProductApiClient productApiClient, IRatingApiClient ratingApiClient)
 		{
 			_productApiClient = productApiClient;
+			_ratingApiClient = ratingApiClient;
 		}
 
 		public IActionResult Index()
@@ -24,6 +28,26 @@ namespace RookieEShop.FrontEnd.Controllers
 		{
 			var products = await _productApiClient.GetProductsById(productId);
 			return View(products);
+		}
+
+		[HttpPost("[controller]/{id}")]
+		public async Task<IActionResult> ProductRating(int val, int productId, string userId)
+		{
+			RatingCreateRequest ratingCreateRequest = new RatingCreateRequest
+			{
+				Val = val,
+				ProductId = productId,
+				UserId = userId
+			};
+
+			var productRating = await _ratingApiClient.PostRating(ratingCreateRequest);
+
+			if (!productRating)
+			{
+				return NoContent();
+			}
+
+			return RedirectToAction("Details", "Product", new { id = productId });
 		}
 	}
 }
