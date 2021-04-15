80efc1a9566b61fc8c052a68a52d05d7218a6f57
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-15 19:05:06 +0700

    Add Rating Httpclient in FrontEnd

---
+++ RookieEShop/RookieEShop.FrontEnd/Services/IRatingApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RookieEShop.Shared;
+
+namespace RookieEShop.FrontEnd.Services
+{
+	public interface IRatingApiClient
+	{
+		Task<bool> PostRating(RatingCreateRequest ratingCreateRequest);
+	}
+}
---
+++ RookieEShop/RookieEShop.FrontEnd/Services/RatingApiClient.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.FrontEnd.Services
+{
+	public class RatingApiClient : IRatingApiClient
+	{
+		private readonly IHttpClientFactory _factory;
+
+		public RatingApiClient(IHttpClientFactory factory)
+		{
+			_factory = factory;
+		}
+
+		public async Task<bool> PostRating(RatingCreateRequest ratingCreateRequest)
+		{
+			var client = _factory.CreateClient();
+
+			var json = JsonConvert.SerializeObject(ratingCreateRequest);
+
+			var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+			var response = await client.PostAsync("https://localhost:44305/api/Rating", data);
+
+			response.EnsureSuccessStatusCode();
+
+			if (response.ReasonPhrase.Equals("GetRating"))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
