93ec656f96ab44ce5616c14e8b7843b01d51b84e
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-17 14:55:52 +0700

    Add Admin project

---
+++ RookieEShop/RookieEShop.Test/SqliteInMemoryFixture.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+using RookieEShop.BackEnd.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RookieEShop.BackEnd.Tests
+{
+	public class SqliteInMemoryFixture : IDisposable
+    {
+        private IServiceScope _serviceScope;
+        private SqliteConnection _connection;
+
+        public virtual IServiceProvider ServiceProvider
+        {
+            get
+            {
+                if (_serviceScope == null)
+                {
+                    _serviceScope = ConfigureServices(new ServiceCollection()).BuildServiceProvider().CreateScope();
+                }
+
+                return _serviceScope.ServiceProvider;
+            }
+        }
+
+        public virtual ApplicationDbContext Context
+            => ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+        public virtual void CreateDatabase()
+        {
+            Dispose();
+            _connection = new SqliteConnection("Data Source=:memory:");
+            _connection.Open();
+            Context.Database.EnsureCreated();
+        }
+
+        public virtual void Dispose()
+        {
+            _connection?.Close();
+            _connection?.Dispose();
+            _serviceScope?.Dispose();
+            _serviceScope = null;
+        }
+
+        public virtual IServiceCollection ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(b => b.UseSqlite(_connection));
+            return services;
+        }
+    }
+}
