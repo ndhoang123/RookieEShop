2da0b47461fa6e4d816405fbb521e4b5ccb6f083
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-25 21:58:13 +0700

    Add Home, and custom UI.

--- RookieEShop/admin/src/App.js
+++ RookieEShop/admin/src/App.js
@@ -1,40 +1,14 @@
 // in src/App.js
 import * as React from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import history from './helpers/history';
-import { LIST_CATEGORY } from "./constants/page";
 import "bootstrap/dist/css/bootstrap.min.css";
-import ListCategory from "./pages/categories/ListCategories";
-import EditCategories from "./pages/categories/EditCategories";
-import ListProduct from "./pages/products/ListProducts";
-import EditProducts from "./pages/products/EditProducts";
-import ListUser from "./pages/users/ListUsers"
+import Home from "./pages/home";
+import "./App.css";
 
 
 const App = () => (
-  <Router history={history}>
-    <Switch>
-      <Route exact path="/">
-        <ListCategory />
-      </Route>
-
-      <Route exact path={["/EditCategory/:id", "/NewCategory"]}>
-        <EditCategories />
-      </Route>
-      
-      <Route exact path="/Product">
-        <ListProduct />
-      </Route>
-
-      <Route exact path={["/EditProduct/:id", "/NewProduct"]}>
-        <EditProducts />
-      </Route>
-
-      <Route exact path="/User">
-        <ListUser />
-      </Route>
-    </Switch>
-  </Router>
+  <div>
+    <Home />
+  </div>
 )
 
 export default App;
--- RookieEShop/admin/src/components/Sidebar.js
+++ RookieEShop/admin/src/components/Sidebar.js
@@ -1,62 +1,30 @@
-import React from 'react';
-import {
-  CDBSidebar,
-  CDBSidebarContent,
-  CDBSidebarFooter,
-  CDBSidebarHeader,
-  CDBSidebarMenu,
-  CDBSidebarMenuItem,
-} from 'cdbreact';
-import { NavLink } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { Nav, NavItem, NavLink } from "reactstrap";
 
-const Sidebar = () => {
-    return (
-        <div
-          style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}
-        >
-            <CDBSidebar textColor="#fff" backgroundColor="#333">
-                <CDBSidebarContent className="sidebar-content">
-                    <CDBSidebarMenu>
-                    <NavLink exact to="/" activeClassName="activeClicked">
-                        <CDBSidebarMenuItem icon="columns">Dashboard</CDBSidebarMenuItem>
-                    </NavLink>
-                    <NavLink exact to="/tables" activeClassName="activeClicked">
-                        <CDBSidebarMenuItem icon="table">Tables</CDBSidebarMenuItem>
-                    </NavLink>
-                    <NavLink exact to="/profile" activeClassName="activeClicked">
-                        <CDBSidebarMenuItem icon="user">Profile page</CDBSidebarMenuItem>
-                    </NavLink>
-                    <NavLink exact to="/analytics" activeClassName="activeClicked">
-                        <CDBSidebarMenuItem icon="chart-line">
-                        Analytics
-                        </CDBSidebarMenuItem>
-                    </NavLink>
-
-                    <NavLink
-                        exact
-                        to="/hero404"
-                        target="_blank"
-                        activeClassName="activeClicked"
-                    >
-                        <CDBSidebarMenuItem icon="exclamation-circle">
-                        404 page
-                        </CDBSidebarMenuItem>
-                    </NavLink>
-                </CDBSidebarMenu>
-            </CDBSidebarContent>
-            <CDBSidebarFooter style={{ textAlign: 'center' }}>
-              <div
-                className="sidebar-btn-wrapper"
-                style={{
-                  padding: '20px 5px',
-                }}
-              >
-                Sidebar Footer
-              </div>
-            </CDBSidebarFooter>
-          </CDBSidebar>
-        </div>
-      );    
-};
-
-export default Sidebar;
+export default function SideBar() {
+  return (
+    <Nav vertical>
+      <NavItem>
+        <NavLink>
+          <Link className="text-decoration-none" to="/Product">
+            Products
+          </Link>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink>
+          <Link className="text-decoration-none" to="/">
+            Categories
+          </Link>
+        </NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink>
+          <Link className="text-decoration-none" to="/User">
+            Users
+          </Link>
+        </NavLink>
+      </NavItem>
+    </Nav>
+  );
+}
