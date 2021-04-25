6e1aee154d8ae0fc236f85e3b5eb7d8239522f07
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-25 21:41:56 +0700

    Create constants folder

---
+++ RookieEShop/admin/src/constants/page.js
@@ -0,0 +1,10 @@
+export const LIST_PRODUCT = '/product';
+export const EDIT_PRODUCT = '/editProduct/:id';
+export const CREATE_PRODUCT = '/editProduct/';
+
+export const HOME_PAGE = '/';
+export const LIST_USER = '/user';
+
+export const LIST_CATEGORY = '/category';
+export const EDIT_CATEGORY = '/editCategory/:id';
+export const CREATE_CATEGORY = '/editCategory/';
