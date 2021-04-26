c1190f83d9745120a0019a48eb158b99e4529c0d
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-26 21:54:12 +0700

    [Admin] Change "config" to "axiosConfig" name in file

---
+++ RookieEShop/admin/src/pages/anxiosConfig.js
@@ -0,0 +1,9 @@
+import Axios from 'axios'
+import host from '../config';
+
+const anxiosConfig = Axios.create({
+    baseURL: host,
+    // baseURL: "https://rookieeshop.azurewebsites.net/",
+  });
+  
+export default anxiosConfig;
