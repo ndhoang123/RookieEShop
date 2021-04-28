656a8f34a5c2cf6ebdcf2ce56efc1fe78c4ebc40
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-28 13:36:29 +0700

    [FrontEnd] Fix "after logout on frontend, the page cannot move back to frontend" bug

--- RookieEShop/RookieEShop.BackEnd/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ RookieEShop/RookieEShop.BackEnd/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using RookieEShop.BackEnd.IdentityServer;
 using RookieEShop.BackEnd.Models;
 
 namespace RookieEShop.BackEnd.Areas.Identity.Pages.Account
@@ -16,25 +18,49 @@ namespace RookieEShop.BackEnd.Areas.Identity.Pages.Account
     {
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly IIdentityServerInteractionService _interaction;
 
-        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<User> signInManager, ILogger<LogoutModel> logger, IIdentityServerInteractionService iteraction)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _interaction = iteraction;
         }
 
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            return await this.OnPost(returnUrl);
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+
+            var logoutId = this.Request.Query["logoutId"].ToString();
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
             }
+
+            else if (!string.IsNullOrEmpty(logoutId))
+			{
+                var logoutContext = await this._interaction.GetLogoutContextAsync(logoutId);
+                returnUrl = logoutContext.PostLogoutRedirectUri;
+
+				if (!string.IsNullOrEmpty(returnUrl))
+				{
+                    return this.Redirect(returnUrl);
+				}
+
+				else
+				{
+                    var clientIdTemp = logoutContext.ClientIds.ToArray()[0];
+                    var referrer = IdentityServerConfig.Clients.Where(item => item.ClientId == clientIdTemp).First();
+                    if (referrer != null) return this.Redirect(referrer.FrontChannelLogoutUri);
+                    return Page();
+                }
+			}
             else
             {
                 return RedirectToPage();
