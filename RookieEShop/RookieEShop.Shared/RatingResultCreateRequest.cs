a5f3e9786b36420dab1229acdb91cc936ef55b8d
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-04-28 11:06:47 +0700

    [RookieEShop.Shared] Add Rating Result view model

---
+++ RookieEShop/RookieEShop.Shared/RatingResultCreateRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.Shared
+{
+	public class RatingResultCreateRequest
+	{
+		public double AvgResult { get; set; }
+
+		public int CountResult { get; set; }
+	}
+}
