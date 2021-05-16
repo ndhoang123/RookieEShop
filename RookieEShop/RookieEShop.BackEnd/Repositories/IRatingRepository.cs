ac51e86b9540da11143c85b39cb9175a5197914c
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-05-16 15:37:46 +0700

    Add RatingRepository

---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/IRatingRepository.cs
@@ -0,0 +1,20 @@
+﻿using RookieEShop.BackEnd.Models;
+using RookieEShop.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public interface IRatingRepository
+	{
+		public Task<IEnumerable<Rating>> GetAllRating();
+
+		public Task<Rating> GetRatingById(int id);
+
+		public Task<RatingResultVm> GetRatingResult(int productId);
+
+		public Task<bool> CreateRating(Rating rating);
+
+		public Task<bool> DeleteRating(int id);
+	}
+}
