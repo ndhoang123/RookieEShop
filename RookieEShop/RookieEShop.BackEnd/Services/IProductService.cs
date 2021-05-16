3d984c210bfe02810473f2059033a34477b0ce91
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-05-16 16:55:46 +0700

    Add ProductService

---
+++ RookieEShop/RookieEShop.BackEnd/Services/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using RookieEShop.BackEnd.Models;
+using RookieEShop.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public interface IProductService
+	{
+		public Task<IEnumerable<Product>> ListAllProduct();
+
+		public Task<Product> ListDetailProduct(int id);
+
+		public Task<IList<ProductVm>> GetProductByCategory(int categoryiD);
+
+		public Task<bool> CreateProduct(Product product);
+
+		public Task<bool> EditProduct(int id, Product product);
+
+		public Task<bool> DeleteProduct(int id);
+	}
+}
---
+++ RookieEShop/RookieEShop.BackEnd/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using RookieEShop.BackEnd.Models;
+using RookieEShop.BackEnd.Repositories;
+using RookieEShop.Shared;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public class ProductService : IProductService
+	{
+		private readonly IProductRepository _productRepository;
+
+		public ProductService(IProductRepository productRepository)
+		{
+			_productRepository = productRepository;
+		}
+
+		public async Task<IEnumerable<Product>> ListAllProduct()
+		{
+			return await _productRepository.ListAllProduct();
+		}
+
+		public async Task<Product> ListDetailProduct(int id)
+		{
+			return await _productRepository.ListDetailProduct(id);
+		}
+
+		public async Task<IList<ProductVm>> GetProductByCategory(int categoryiD)
+		{
+			return await _productRepository.GetProductByCategory(categoryiD);
+		}
+
+		public async Task<bool> CreateProduct(Product product)
+		{
+			return await _productRepository.CreateProduct(product);
+		}
+
+		public async Task<bool> EditProduct(int id, Product product)
+		{
+			return await _productRepository.EditProduct(id, product);
+		}
+
+		public async Task<bool> DeleteProduct(int id)
+		{
+			return await _productRepository.DeleteProduct(id);
+		}
+	}
+}
