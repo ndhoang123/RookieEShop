6d0202c9fcc0a815d1937cc6df412f9e49750113
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-05-16 15:38:07 +0700

    Add RatingService

---
+++ RookieEShop/RookieEShop.BackEnd/Services/RatingService.cs
@@ -0,0 +1,60 @@
+﻿using RookieEShop.BackEnd.Models;
+using RookieEShop.BackEnd.Repositories;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public class RatingService : IRatingService
+	{
+		private readonly IRatingRepository _ratingRepository;
+
+		public RatingService(IRatingRepository ratingRepository)
+		{
+			_ratingRepository = ratingRepository;
+		}
+
+		public async Task<IEnumerable<Rating>> ListRating()
+		{
+			return await _ratingRepository.GetAllRating();
+		}
+
+		public async Task<Rating> DetailRating(int id)
+		{
+			return await _ratingRepository.GetRatingById(id);
+		}
+
+		public async Task<RatingResultVm> TotalRating(int productId)
+		{
+			return await _ratingRepository.GetRatingResult(productId);
+		}
+
+		public async Task<bool> CreateRating(Rating rating)
+		{
+			if (await _ratingRepository.CreateRating(rating))
+			{
+				return true;
+			}
+
+			else
+			{
+				return false;
+			}
+		}
+
+		public async Task<bool> DeleteRating(int id)
+		{
+			if(await _ratingRepository.DeleteRating(id))
+			{
+				return true;
+			}
+			else
+			{
+				 return false;
+			}
+		}
+	}
+}
