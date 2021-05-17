cf3e1ab8845b4916525a5923d94df99e1c118e2b
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-05-17 23:03:35 +0700

    Add ProductEditRequest file

---
+++ RookieEShop/RookieEShop.Shared/ProductEditRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.Shared
+{
+	public class ProductEditRequest
+	{
+        [Required]
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Author { get; set; }
+
+        public int Year { get; set; }
+
+        public string Description { get; set; }
+
+        public string Publisher { get; set; }
+
+        public bool IsDisableProduct { get; set; }
+    }
+}
