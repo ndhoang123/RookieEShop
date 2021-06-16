fad73de0abf7ff3b5622307438b86c885d5df0de
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-06-16 15:04:11 +0700

    Custom App UI

--- RookieEShop/admin/src/App.js
+++ RookieEShop/admin/src/App.js
@@ -3,12 +3,21 @@ import * as React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/home";
 import "./App.css";
-
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SideBar from "./components/Sidebar";
+import { BrowserRouter as Router } from "react-router-dom";
+import PageLayout from "./containers/PageLayout";
+import Routes from "./route";
 
 const App = () => (
-  <div>
-    <Home />
-  </div>
+  <Router>
+    <PageLayout 
+      header={<Header/>}
+      nav={<SideBar footer={<Footer />}/>}
+      content={<Routes/>}
+    />
+  </Router>
 )
 
 export default App;
