5bcf08000dc37dbb2ada9ed38827169df7132d0f
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-06-17 10:54:34 +0700

    Custom Header

--- RookieEShop/admin/src/components/Header.js
+++ RookieEShop/admin/src/components/Header.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { Button } from "reactstrap";
 //
-// import { useSelector } from "react-redux";
-// import { signoutRedirect } from "../services/authService";
+import { useSelector } from "react-redux";
+import { signoutRedirect } from "../services/authService";
 
 export default function Header() {
-  // const user = useSelector((state) => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
 
-  // const signOut = () => signoutRedirect();
+  const signOut = () => signoutRedirect();
 
   return (
     <div className="clearfix">
       <div className="float-left">
         <img width="40" src="./logo192.png" alt="" />
       </div>
-      <div className="float-right ">
-        {/* <span>Hello, {user?.profile.name}</span> */}
-        <Button
-          color="link"
-          // onClick={signOut}
-          className="pl-3 text-danger"
-          size="sm"
-        >
-          Sign Out
-        </Button>
-      </div>
+      {user && (
+        <div className="float-right ">
+          <span>Hello, {user?.profile.name}</span>
+          <Button
+            color="link"
+            onClick={signOut}
+            className="pl-3 text-danger"
+            size="sm"
+          >
+            Sign Out
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
--- RookieEShop/admin/src/store.js
+++ RookieEShop/admin/src/store.js
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
+import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const initalState = {};
