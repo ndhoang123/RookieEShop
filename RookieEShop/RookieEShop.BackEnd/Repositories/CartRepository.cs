32a66d6e6494072c59a31ad6ef505039b033f5dc
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-07-01 11:09:19 +0700

    Add Get in Cart Repo

---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/CartRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RookieEShop.BackEnd.Data;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public class CartRepository : ICartRepository
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public CartRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<IEnumerable<CartVm>> GetAllCart()
+		{
+			var listAllCart = await _dbContext.Carts.Select(x => new CartVm
+			{
+				Id = x.Id,
+				ProductName = x.Product.Name,
+				UserName = x.User.UserName
+			})
+				.AsNoTracking()
+				.ToListAsync();
+
+			return listAllCart;
+		}
+
+		public async Task<CartVm> GetDetailCart(int id)
+		{
+			var detailCart = await _dbContext.Carts.Select(x => new CartVm
+			{
+				Id = x.Id,
+				ProductName = x.Product.Name,
+				UserName = x.User.UserName
+			})
+				.Where(item => item.Id.Equals(id))
+				.AsNoTracking()
+				.SingleAsync();
+
+			return detailCart;
+		}
+	}
+}
---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/ICartRepository.cs
@@ -0,0 +1,15 @@
+﻿using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public interface ICartRepository
+	{
+		Task<IEnumerable<CartVm>> GetAllCart();
+
+		Task<CartVm> GetDetailCart(int id);
+	}
+}
