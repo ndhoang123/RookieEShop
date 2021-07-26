40918a53954a42014613de31c78fecf0361a959f
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-07-26 17:15:32 +0700

    Add Cart Api Client

---
+++ RookieEShop/RookieEShop.FrontEnd/Services/ICartApiClient.cs
@@ -0,0 +1,19 @@
+﻿using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.FrontEnd.Services
+{
+	public interface ICartApiClient
+	{
+		Task<IList<CartVm>> GetAllCarts();
+
+		Task<IList<CartVm>> GetDetailCart(int id);
+
+		Task<bool> AddNewItem(CartCreateRequest request);
+
+		Task<bool> UpdateItem(int id, CartEdit edit);
+	}
+}
