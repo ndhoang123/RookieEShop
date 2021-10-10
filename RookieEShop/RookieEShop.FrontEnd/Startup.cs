ae91e4a74a953862dd91cf426ac42dfaf50822dc
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-10-10 16:39:26 +0700

    [FE] Add library to FE

--- RookieEShop/RookieEShop.FrontEnd/Startup.cs
+++ RookieEShop/RookieEShop.FrontEnd/Startup.cs
@@ -40,6 +40,8 @@ namespace RookieEShop.FrontEnd
 			services.AddSession(options =>
 			{
 				options.IdleTimeout = TimeSpan.FromMinutes(30);
+				options.Cookie.HttpOnly = true;
+				options.Cookie.IsEssential = true;
 			});
 		}
 
