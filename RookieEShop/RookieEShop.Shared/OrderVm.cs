231782aa58a9375628a5e4e44a6f21abfcfad431
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-10-18 15:50:43 +0700

    [Shared] Add OrderVm

---
+++ RookieEShop/RookieEShop.Shared/OrderVm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.Shared
+{
+	public class OrderVm
+	{
+		public string ClientName { get; set; }
+
+		public int Phone { get; set; }
+
+		public string Address { get; set; }
+
+		public int TotalMoney { get; set; }
+
+		public int NumberOfStuff { get; set; }
+	}
+}
