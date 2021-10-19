ff6604bddaed5e9f831f884354710c8dc20c2da8
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-10-19 09:38:08 +0700

    [BE] Add Order Service

---
+++ RookieEShop/RookieEShop.BackEnd/Services/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using RookieEShop.BackEnd.Models;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public interface IOrderService
+	{
+		Task<bool> AddOrder(Ordering order);
+	}
+}
---
+++ RookieEShop/RookieEShop.BackEnd/Services/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using RookieEShop.BackEnd.Models;
+using RookieEShop.BackEnd.Repositories;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public class OrderService : IOrderService
+	{
+		private readonly IOrderRepository _orderRepo;
+
+		public OrderService(IOrderRepository orderRepo)
+		{
+			_orderRepo = orderRepo;
+		}
+
+		public async Task<bool> AddOrder(Ordering order)
+		{
+			return await _orderRepo.AddOrder(order);
+		}
+	}
+}
