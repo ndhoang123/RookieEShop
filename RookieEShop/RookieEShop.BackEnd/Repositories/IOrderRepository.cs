0e44c4a23d3af1193c81f04243a5956d5640e35c
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-10-19 09:38:34 +0700

    [BE] Add Order repo

---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/IOrderRepository.cs
@@ -0,0 +1,10 @@
+﻿using RookieEShop.BackEnd.Models;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public interface IOrderRepository
+	{
+		Task<bool> AddOrder(Ordering order);
+	}
+}
---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/OrderRepository.cs
@@ -0,0 +1,31 @@
+﻿using RookieEShop.BackEnd.Data;
+using RookieEShop.BackEnd.Models;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public class OrderRepository : IOrderRepository
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public OrderRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<bool> AddOrder(Ordering order)
+		{
+			_dbContext.Orderings.Add(order);
+
+			if(await _dbContext.SaveChangesAsync() > 0)
+			{
+				return true;
+			}
+
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
