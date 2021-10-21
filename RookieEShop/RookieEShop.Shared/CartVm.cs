6e90d1fd7fda73f3614aec2ee4f7789b5cca5fbb
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-10-21 15:33:39 +0700

    [Shared] Add Image in CartVm

--- RookieEShop/RookieEShop.Shared/CartVm.cs
+++ RookieEShop/RookieEShop.Shared/CartVm.cs
@@ -14,6 +14,8 @@ namespace RookieEShop.Shared
 
 		public string ProductName { get; set; }
 
+		public string ThumbnailImageUrl { get; set; }
+
 		public int Quantity { get; set; }
 
 		public decimal Price { get; set; }
