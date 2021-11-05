76ca8141fa19514be071a5f56879d6c32dd9f9b3
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-05 16:42:43 +0700

    [BE] Update Db

--- RookieEShop/RookieEShop.BackEnd/Migrations/20211105093916_DeleteCartDb.Designer.cs
+++ RookieEShop/RookieEShop.BackEnd/Migrations/20211105093916_DeleteCartDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RookieEShop.BackEnd.Data;
 
 namespace RookieEShop.BackEnd.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211105093916_DeleteCartDb")]
+    partial class DeleteCartDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
