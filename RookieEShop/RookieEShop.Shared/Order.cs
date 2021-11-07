232ee45e1b4264fe4cb51d8fe8098042e9b70aea
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-07 22:35:21 +0700

    [Vm] Add Order

---
+++ RookieEShop/RookieEShop.Shared/Order.cs
@@ -0,0 +1,21 @@
+﻿namespace RookieEShop.Shared
+{
+	public class Order
+	{
+		public int Id { get; set; }
+
+		public string Status { get; set; }
+
+		public string TotalMoney { get; set;  }
+
+		public string CreatedAt { get; set; }
+
+		public int NumOfStuff { get; set; }
+
+		public string ClientName { get; set; }
+
+		public int Phone { get; set; }
+
+		public string Address { get; set; }
+	}
+}
