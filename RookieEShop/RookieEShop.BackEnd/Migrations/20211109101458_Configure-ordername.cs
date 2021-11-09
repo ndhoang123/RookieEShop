f2cfd5116e9c4258b957bb868deffc3b7be2dddd
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-09 17:18:48 +0700

    [BE] Add ordername column configuration

---
+++ RookieEShop/RookieEShop.BackEnd/Migrations/20211109101458_Configure-ordername.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RookieEShop.BackEnd.Migrations
+{
+    public partial class Configureordername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderName",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                computedColumnSql: "N'RE'+ RIGHT('0000000'+CAST(OrderDetailId AS VARCHAR(7)),7)",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OrderName",
+                table: "OrderDetails",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldComputedColumnSql: "N'RE'+ RIGHT('0000000'+CAST(OrderDetailId AS VARCHAR(7)),7)");
+        }
+    }
+}
