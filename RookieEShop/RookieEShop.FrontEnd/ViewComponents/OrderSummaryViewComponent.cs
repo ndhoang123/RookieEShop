4181185d49f702f015576821488b0848a214348f
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-11 11:19:21 +0700

    [FE] Add ViewComponent for OrderSummary

---
+++ RookieEShop/RookieEShop.FrontEnd/ViewComponents/OrderSummaryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RookieEShop.FrontEnd.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.FrontEnd.ViewComponents
+{
+	public class OrderSummaryViewComponent : ViewComponent
+	{
+		private readonly ICartApiClient _cartApiClient;
+
+		public OrderSummaryViewComponent(ICartApiClient cartApiClient)
+		{
+			_cartApiClient = cartApiClient;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var cart = await _cartApiClient.GetAllCart();
+			return View(cart);
+		}
+	}
+}
