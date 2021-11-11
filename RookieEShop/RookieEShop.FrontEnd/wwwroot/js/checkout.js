8226a510201903024f7f8c9d65d88086bad27249
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-11 16:52:13 +0700

    [FE] Add Js for address button in checkout

---
+++ RookieEShop/RookieEShop.FrontEnd/wwwroot/js/checkout.js
@@ -0,0 +1,15 @@
+﻿$(function(){
+	function toggleCreateShippingAddress() {
+		var $createShipping = $('.create-shipping-address'),
+			shippingAddressId = $('input[name=ShippingAddressId]:checked').val();
+
+		if (shippingAddressId === "0") {
+			$createShipping.show();
+		}
+		else {
+			$createShipping.hide();
+		}
+	}
+
+	toggleCreateShippingAddress();
+})
