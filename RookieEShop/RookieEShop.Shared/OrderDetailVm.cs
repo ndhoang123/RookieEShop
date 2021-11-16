6d0d82c9bfa1e76b24487aedf32af62b0578bf38
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-16 14:32:56 +0700

    [Viewmodel] Add OrderDetailVm

---
+++ RookieEShop/RookieEShop.Shared/OrderDetailVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieEShop.Shared
+{
+	public class OrderDetailVm
+	{
+		public DateTime DeliveryDate { get; set; }
+
+		public DateTime BillDate { get; set; }
+
+		public decimal Price { get; set; }
+
+		public string StatusCart { get; set; }
+
+		public int Qty { get; set; }
+
+		public List<int> ProductId { get; set; }
+	}
+}
