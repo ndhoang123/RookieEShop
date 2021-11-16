67a823264169a3a6ddf50bcbb863aa520ab63378
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-16 14:23:42 +0700

    [ViewModel] Update OrderVm

--- RookieEShop/RookieEShop.Shared/OrderVm.cs
+++ RookieEShop/RookieEShop.Shared/OrderVm.cs
@@ -5,19 +5,17 @@ namespace RookieEShop.Shared
 	public class OrderVm
 	{
 		public string ClientName { get; set; }
-
+		
 		public int Phone { get; set; }
 
 		public string Address { get; set; }
 
 		public int TotalMoney { get; set; }
 
-		public int NumberOfStuff { get; set; }
-
 		public string UserId { get; set; }
 
 		public DateTime CreatedAt { get; set; }
 
-		public string Status { get; set; }
+		public int Id { get; set; }
 	}
 }
