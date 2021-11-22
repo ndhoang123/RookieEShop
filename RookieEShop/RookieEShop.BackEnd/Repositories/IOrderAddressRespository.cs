3db258858f0fca85722190e745b99037ba90f390
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-22 16:28:35 +0700

    [BE] Add GetCurrentAddress in Repo

---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/IOrderAddressRespository.cs
@@ -0,0 +1,13 @@
+﻿using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public interface IOrderAddressRespository
+	{
+		public Task<OrderAddressVm> GetCurrentAddress(long id);
+	}
+}
---
+++ RookieEShop/RookieEShop.BackEnd/Repositories/OrderAddressRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RookieEShop.BackEnd.Data;
+using RookieEShop.Shared;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Repositories
+{
+	public class OrderAddressRepository : IOrderAddressRespository
+	{
+		private readonly ApplicationDbContext _dbContext;
+
+		public OrderAddressRepository(ApplicationDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		public async Task<OrderAddressVm> GetCurrentAddress(long id)
+		{
+			var list = await _dbContext.OrderAddresses.Where(x => x.Id.Equals(id))
+												.AsNoTracking()
+												.SingleAsync();
+
+			if (list == null)
+			{
+				return null;
+			}
+
+			var listVm = new OrderAddressVm
+			{
+				HomeAddress = list.Address,
+				ContactName = list.Name,
+				Id = list.Id,
+				OrderCity = list.City,
+				OrderDistrict = list.District,
+				PhoneNumber = list.Phone,
+			};
+
+			return listVm;
+		}
+	}
+}
