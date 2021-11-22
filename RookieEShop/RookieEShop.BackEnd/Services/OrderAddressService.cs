d7a2e4909507b54b8a2d94f2a9d1f50e889d8856
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-22 16:28:05 +0700

    [BE] Add GetCurrentAddress in Service

---
+++ RookieEShop/RookieEShop.BackEnd/Services/OrderAddressService.cs
@@ -0,0 +1,24 @@
+﻿using RookieEShop.BackEnd.Repositories;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public class OrderAddressService : IOrderAddressService
+	{
+		private readonly IOrderAddressRespository _orderAddress;
+
+		public OrderAddressService(IOrderAddressRespository orderAddress)
+		{
+			_orderAddress = orderAddress;
+		}
+
+		public async Task<OrderAddressVm> GetCurrentAddress(long id)
+		{
+			return await _orderAddress.GetCurrentAddress(id);
+		}
+	}
+}
