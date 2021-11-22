57ebb3125678fa079033832b24648312d0107e25
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-22 16:27:15 +0700

    [BE] Modify the GetAddressShipping method

--- RookieEShop/RookieEShop.BackEnd/Repositories/OrderRepository.cs
+++ RookieEShop/RookieEShop.BackEnd/Repositories/OrderRepository.cs
@@ -20,17 +20,57 @@ namespace RookieEShop.BackEnd.Repositories
 		public async Task<IEnumerable<OrderVm>> GetAddressShipping(string userId)
 		{
 			var listOrder = await _dbContext.Orderings
+									.Include(x => x.OrderDetail)
+									.Include(x => x.ShippingAddress)
 									.Where(x => x.UserId.Equals(userId))
-									.Select(x => new OrderVm
-									{
-										Address = x.Address,
-										ClientName = x.Name,
-										Phone = x.Phone,
-										CreatedAt = x.CreatedAt
-									})
 									.AsNoTracking()
 									.ToListAsync();
-			return listOrder;
+
+			List<OrderDetailVm> getDetails = new List<OrderDetailVm>();
+			foreach(var i in listOrder)
+			{
+				foreach(var k in i.OrderDetail)
+				{
+					getDetails.Add(new OrderDetailVm
+					{
+						Qty = k.Qty,
+						Discount = k.Discount,
+						ProductId = k.ProductId,
+						Price = k.Price
+					});
+				}
+			}
+
+			var listVm = listOrder.Select(x => new OrderVm
+			{
+				OrderId = x.Id,
+				UserId = x.UserId,
+				CreatedAt = x.CreatedAt,
+				BillDay = x.BillDate,
+				Coupon = x.CouponName,
+				DeliveryDay = x.DeliveryDate,
+				Note = x.OrderNote,
+				StatusCart = x.StatusCart,
+				TaxAmount = x.TaxAmount,
+				ShippingMethod = x.ShippingMethod,
+				PaymentFee = x.PaymentFee,
+				ShippingFee = x.ShippingFee,
+				PaymentMethod = x.PaymentMethod,
+				ShippingAddressId = x.ShippingAddressId,
+				OrderName = x.OrderName,
+				OrderDetail = getDetails,
+				OrderAddressForm = new OrderAddressVm
+				{
+					ContactName = x.ShippingAddress.Name,
+					HomeAddress = x.ShippingAddress.Address,
+					OrderCity = x.ShippingAddress.City,
+					OrderDistrict = x.ShippingAddress.District,
+					PhoneNumber = x.ShippingAddress.Phone
+				}
+			}).ToList();
+
+
+			return listVm;
 		}
 
 		public async Task<bool> AddOrder(Ordering order)
