b9a5d10d51aa4019405d08fe03de3c15cd8ff6c7
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-28 10:34:07 +0700

    [FE] Add CheckoutApiClient

---
+++ RookieEShop/RookieEShop.FrontEnd/Services/CheckoutApiClient.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RookieEShop.FrontEnd.Services
+{
+	public class CheckoutApiClient : ICheckoutApiClient
+	{
+		private readonly HttpClient _client;
+		private readonly IHttpClientFactory _httpClientFactory;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+
+		public CheckoutApiClient(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor)
+		{
+			_httpClientFactory = httpClientFactory;
+			_httpContextAccessor = httpContextAccessor;
+			_client = _httpClientFactory.CreateClient("owner");
+		}
+
+		public async Task<DeliveryInformationVm> GetShipping()
+		{
+			var userId = _httpContextAccessor.HttpContext.User.FindFirstValue("sub");
+
+			var response = await _client.GetAsync("api/Checkout/" + userId.ToString());
+
+			response.EnsureSuccessStatusCode();
+
+			return await response.Content.ReadAsAsync<DeliveryInformationVm>();
+		}
+	}
+}
