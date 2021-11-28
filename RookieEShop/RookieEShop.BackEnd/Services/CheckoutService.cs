46f4a86eed095c32e3adde8796c344ced0f292d0
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-28 10:35:29 +0700

    [BE] Add CheckoutService

---
+++ RookieEShop/RookieEShop.BackEnd/Services/CheckoutService.cs
@@ -0,0 +1,24 @@
+﻿using RookieEShop.BackEnd.Repositories;
+using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public class CheckoutService : ICheckoutService
+	{
+		private readonly ICheckOutRepository _ICheckoutRepo;
+
+		public CheckoutService(ICheckOutRepository checkoutRepository)
+		{
+			_ICheckoutRepo = checkoutRepository;
+		}
+
+		public async Task<DeliveryInformationVm> GetShipping(string userId)
+		{
+			return await _ICheckoutRepo.GetShipping(userId);
+		}
+ 	}
+}
---
+++ RookieEShop/RookieEShop.BackEnd/Services/ICheckoutService.cs
@@ -0,0 +1,13 @@
+﻿using RookieEShop.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.BackEnd.Services
+{
+	public interface ICheckoutService
+	{
+		Task<DeliveryInformationVm> GetShipping(string userId);
+	}
+}
