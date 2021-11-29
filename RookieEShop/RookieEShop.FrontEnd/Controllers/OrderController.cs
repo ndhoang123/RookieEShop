dbbac017b6a87f58b0172cc321ed0425a08ac39f
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-11-29 11:45:20 +0700

    [FE] Add OrderController

---
+++ RookieEShop/RookieEShop.FrontEnd/Controllers/OrderController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RookieEShop.FrontEnd.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RookieEShop.FrontEnd.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IOrderApiClient _orderApiClient;
+
+		public OrderController(IOrderApiClient orderApiClient)
+		{
+			_orderApiClient = orderApiClient;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> History()
+		{
+			var history = await _orderApiClient.GetHistory();
+			return View(history);
+		}
+
+		[Route("history/{id:int}", Name ="history")]
+		public async Task<IActionResult> Detail(int id)
+		{
+			var history = await _orderApiClient.GetHistory();
+			var detail = history.Where(x => x.OrderId.Equals(id));
+			return View(detail);
+		}
+	}
+}
