f051e7810bd2aa5bf95e1e1677520c1e03591907
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-03 15:27:35 +0700

    [BE] Add migration and update db

---
+++ RookieEShop/RookieEShop.BackEnd/Migrations/20211203082256_updateOrderTracking.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RookieEShop.BackEnd.Migrations
+{
+    public partial class updateOrderTracking : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderTrackings_OrderDetails_OrderDetailId",
+                table: "OrderTrackings");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderDetailId",
+                table: "OrderTrackings",
+                newName: "OrderingId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderTrackings_OrderDetailId",
+                table: "OrderTrackings",
+                newName: "IX_OrderTrackings_OrderingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderTrackings_Orderings_OrderingId",
+                table: "OrderTrackings",
+                column: "OrderingId",
+                principalTable: "Orderings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderTrackings_Orderings_OrderingId",
+                table: "OrderTrackings");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderingId",
+                table: "OrderTrackings",
+                newName: "OrderDetailId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderTrackings_OrderingId",
+                table: "OrderTrackings",
+                newName: "IX_OrderTrackings_OrderDetailId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderTrackings_OrderDetails_OrderDetailId",
+                table: "OrderTrackings",
+                column: "OrderDetailId",
+                principalTable: "OrderDetails",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
