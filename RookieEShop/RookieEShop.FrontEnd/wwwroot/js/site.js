ff95476ad2d904ee7e724a3b06ff0fd1dffa691a
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-10 16:39:53 +0700

    [FE] Add js for home page

--- RookieEShop/RookieEShop.FrontEnd/wwwroot/js/site.js
+++ RookieEShop/RookieEShop.FrontEnd/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const userAvatar = document.getElementById("userAvatar");
+const dropdownMenu = document.getElementById("dropdownMenu");
+
+userAvatar.addEventListener('click', () => {
+	userAvatar.classList.toggle("dropdown-trigger-personalNavActive");
+	dropdownMenu.classList.toggle("dropdownMenu-Show");
+})
