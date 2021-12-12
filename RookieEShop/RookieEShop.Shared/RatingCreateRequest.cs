f11e2a5e1ae83f1885128c6251203178b95fa5cf
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-12 16:04:51 +0700

    [FE] Fix bug that cannot send the userId to RatingAPI in post method

--- RookieEShop/RookieEShop.Shared/RatingCreateRequest.cs
+++ RookieEShop/RookieEShop.Shared/RatingCreateRequest.cs
@@ -8,6 +8,8 @@ namespace RookieEShop.Shared
 {
 	public class RatingCreateRequest
 	{
+		public string userId { get; set; }
+
 		public int Val { get; set; }
 
 		public string Comment { get; set; }
