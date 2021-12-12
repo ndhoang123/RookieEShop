4d6154a37f476836e97f121ad2e1061a3f69d5df
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2021-12-12 15:50:36 +0700

    [FE] Fix error that cannot close the dropdown when clicking the outside of it.

--- RookieEShop/RookieEShop.FrontEnd/wwwroot/js/site.js
+++ RookieEShop/RookieEShop.FrontEnd/wwwroot/js/site.js
@@ -5,7 +5,13 @@
 const userAvatar = document.getElementById("userAvatar");
 const dropdownMenu = document.getElementById("dropdownMenu");
 
-userAvatar.addEventListener('click', () => {
+userAvatar.addEventListener('click', (e) => {
+	e.stopPropagation();
 	userAvatar.classList.toggle("dropdown-trigger-personalNavActive");
 	dropdownMenu.classList.toggle("dropdownMenu-Show");
 })
+
+window.onclick = function(event){
+	dropdownMenu.classList.remove("dropdownMenu-Show");
+	userAvatar.classList.remove("dropdown-trigger-personalNavActive");
+}
