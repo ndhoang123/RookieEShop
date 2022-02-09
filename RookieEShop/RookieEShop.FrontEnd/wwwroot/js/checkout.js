dca64a889e22c9b003af1902e516fffa6773dc48
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-02-09 17:14:47 +0700

    Convert from jquery to js

--- RookieEShop/RookieEShop.FrontEnd/wwwroot/js/checkout.js
+++ RookieEShop/RookieEShop.FrontEnd/wwwroot/js/checkout.js
@@ -1,19 +1,31 @@
-﻿$(function(){
-	function toggleCreateShippingAddress() {
-		var shippingAddressId = $('input[name=ShippingAddressId]:checked').val(),
-			$createShipping = $('.create-shipping-address');
+﻿var shippingAddressId = document.querySelector("input[name=ShippingAddressId]:checked").value;
+const createShipping = document.getElementsByClassName("create-shipping-address");
+const textAddress = document.querySelectorAll("input[type=text]");
+const btn = document.getElementById("addOrder");
 
-		if (shippingAddressId === "0") {
-			$createShipping.show();
-		}
-		else {
-			$createShipping.hide();
-		}
+function toggleCreateShippingAddress(){
+	console.log(shippingAddressId);
+	if(shippingAddressId === "0"){
+		createShipping[0].style.display = "block";
+		textAddress.forEach(function(x){
+			x.required = true;
+		})
 	}
+	if(shippingAddressId !== "0"){
+		console.log("B");
+		createShipping[0].style.display = "none";
+		textAddress.forEach(function(x){
+			x.required = false;
+		})
+	}
+}
 
-	$('input[name=ShippingAddressId]').on('change', function () {
-		toggleCreateShippingAddress();
-	});
-
+document.querySelector('input[name=ShippingAddressId]').addEventListener('click', function(){
 	toggleCreateShippingAddress();
+})
+
+toggleCreateShippingAddress();
+
+btn.addEventListener("click", function(){
+	alert("Hello world")
 })
