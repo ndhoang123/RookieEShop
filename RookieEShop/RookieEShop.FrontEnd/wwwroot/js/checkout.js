0c4e9516c4e03e6b1e21c456bfca793c7ea0be7d
DESKTOP-SMPGKL7\ACER <hoangnguyen12397@gmail.com>
2022-02-09 22:50:19 +0700

    [FE] Fix error that if the new address will demand the input

--- RookieEShop/RookieEShop.FrontEnd/wwwroot/js/checkout.js
+++ RookieEShop/RookieEShop.FrontEnd/wwwroot/js/checkout.js
@@ -1,31 +1,26 @@
-﻿var shippingAddressId = document.querySelector("input[name=ShippingAddressId]:checked").value;
-const createShipping = document.getElementsByClassName("create-shipping-address");
+﻿const createShipping = document.querySelector(".create-shipping-address");
 const textAddress = document.querySelectorAll("input[type=text]");
-const btn = document.getElementById("addOrder");
 
-function toggleCreateShippingAddress(){
-	console.log(shippingAddressId);
-	if(shippingAddressId === "0"){
-		createShipping[0].style.display = "block";
-		textAddress.forEach(function(x){
-			x.required = true;
-		})
-	}
-	if(shippingAddressId !== "0"){
-		console.log("B");
-		createShipping[0].style.display = "none";
-		textAddress.forEach(function(x){
-			x.required = false;
-		})
-	}
+function toggleCreateShippingAddress(addressValue) {
+  createShipping.style.display = addressValue === "0" ? "block" : "none";
+  textAddress.forEach(function (x) {
+    x.required = addressValue === "0";
+  });
 }
 
-document.querySelector('input[name=ShippingAddressId]').addEventListener('click', function(){
-	toggleCreateShippingAddress();
-})
+// for re-render when select the other input.
+var shippingAddressElms = document.querySelectorAll(
+  "input[name=ShippingAddressId]"
+);
+shippingAddressElms.forEach(function (elm) {
+  elm.addEventListener("change", function (event) {
+    toggleCreateShippingAddress(event.target.value);
+  });
+});
 
-toggleCreateShippingAddress();
+// get last input radio.
+// for first render.
+toggleCreateShippingAddress(
+  shippingAddressElms[shippingAddressElms.length - 1].checked ? "0" : ""
+);
 
-btn.addEventListener("click", function(){
-	alert("Hello world")
-})
